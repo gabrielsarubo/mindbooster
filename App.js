1ab274843175b9975999f1fcd98d9ec7e3b9c345
Gabriel <gabrielsarubo@hotmail.com>
2022-04-25 18:41:38 -0300

    setup boilerplate

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler'
 
-export default function App() {
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+
+import LoginScreen from './src/pages/LoginScreen'
+import SigninScreen from './src/pages/SigninScreen'
+
+const Stack = createNativeStackNavigator()
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen 
+            name='Login'
+            component={LoginScreen}
+            options={{ headerShown: false, }}
+          />
+          <Stack.Screen 
+            name='Signin'
+            component={SigninScreen}
+            options={{ headerShown: false, }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+ 
+export default App
---
+++ src/pages/LoginScreen.js
@@ -0,0 +1,13 @@
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+import { Text } from 'react-native'
+
+const LoginScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>Login Screen</Text>
+    </SafeAreaView>
+  )
+}
+ 
+export default LoginScreen
