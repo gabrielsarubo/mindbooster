b3c39129cd9a8c35564367ed0c1f421b792e56be
Gabriel <gabrielsarubo@hotmail.com>
2022-04-26 12:49:43 -0300

    create global styles

---
+++ styles/colors.js
@@ -0,0 +1,8 @@
+const colors = {
+  darkPurple: '#454161',
+  lightPurple: '#6a61a1',
+  pink: '#b58d97',
+  inputBackground: '#fdfdfd',
+}
+
+export default colors
---
+++ styles/global.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native'
+
+import colors from './colors'
+
+export const globalStyles = StyleSheet.create({
+  border: {
+    borderWidth: 1,
+    borderColor: 'red',
+  },
+  
+  bodyContainer: {
+    flex: 1,
+    justifyContent: 'space-between',
+    paddingHorizontal: 32,
+    backgroundColor: colors.lightPurple,
+    color: '#fdfdfd',
+  },  
+})
