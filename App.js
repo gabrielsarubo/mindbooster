5813dfd80637e9d5f146aea3b2c36b4cc7adf497
Gabriel <gabrielsarubo@hotmail.com>
2022-04-29 23:53:40 -0300

    fix: rename HomeStack to AuthStack

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import 'react-native-gesture-handler'
 import { NavigationContainer } from '@react-navigation/native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
-import Navigator from './routes/HomeStack'
+import Navigator from './routes/AuthStack'
 
 const App = () => {
   return (
