1942b0df3a067510af3d050895d52cea74c1f268
Gabriel <gabrielsarubo@hotmail.com>
2022-04-30 20:42:39 -0300

    change styles of the StatusBar

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import 'react-native-gesture-handler'
 
+import { StatusBar } from 'expo-status-bar'
 import { NavigationContainer } from '@react-navigation/native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
@@ -8,6 +9,7 @@ import Navigator from './routes/Drawer'
 const App = () => {
   return (
     <SafeAreaProvider>
+      <StatusBar style='light' />
       <NavigationContainer>
         <Navigator />
       </NavigationContainer>
