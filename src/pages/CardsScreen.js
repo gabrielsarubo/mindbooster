7543f05405b63710d984297954361ccd95baded9
Gabriel <gabrielsarubo@hotmail.com>
2022-05-01 14:09:28 -0300

    refact: create component CardListItem

--- src/pages/CardsScreen.js
+++ src/pages/CardsScreen.js
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react'
 
-import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, FlatList, StyleSheet } from 'react-native'
 
 import CustomTextInput from '../components/CustomTextInput'
 
-import { MaterialIcons } from '@expo/vector-icons'
 import CustomButton from '../components/CustomButton'
+import CardListItem from '../components/CardListItem'
 
 const CardsScreen = ({ route }) => {
 
@@ -34,26 +34,7 @@ const CardsScreen = ({ route }) => {
     setFilteredCards(filteredCards)
   }
 
-  const renderItemCard = ({ item }) => (
-    <TouchableOpacity>
-      <View style={styles.cardListItem}>
-        <View style={styles.wrapperContent}>
-          <View style={styles.content}>
-            <Text style={styles.label}>Frente</Text>
-            <Text style={styles.title} numberOfLines={1}>{item.front}</Text>
-          </View>
-          <View style={styles.content}>
-            <Text style={styles.label}>Verso</Text>
-            <Text style={styles.title} numberOfLines={1}>{item.back}</Text>
-          </View>
-        </View>
-        <View style={styles.wrapperButtons}>
-          <View style={styles.button}><MaterialIcons name='create' size={28} color='#4472C4' /></View>
-          <View style={styles.button}><MaterialIcons name='delete-forever' size={28} color='#E91010' /></View>
-        </View>
-      </View>
-    </TouchableOpacity>
-  )
+  const renderItemCard = ({ item }) => <CardListItem item={item} />
 
   return (
     <View style={styles.container}>
@@ -103,49 +84,6 @@ const styles = StyleSheet.create({
   flatList: {
     paddingBottom: 20,
   },
-
-  // ! CardListItem styles
-  cardListItem: {
-    flexDirection: 'row',
-    backgroundColor: '#fff',
-    justifyContent: 'space-between',
-    borderRadius: 10,
-    paddingHorizontal: 14,
-    paddingVertical: 12,
-    marginBottom: 16,
-  },
-
-  wrapperContent: {
-    flexDirection: 'row',
-  },
-
-  content: {
-    width: 100,
-    maxWidth: 100,
-    marginRight: 16,
-  },
-
-  label: {
-    color: '#a5a5a5',
-    fontSize: 13,
-    letterSpacing: 0,
-  },
-
-  title: {
-    fontWeight: '700',
-    fontSize: 24,
-    color: '#27ACA7',
-  },
-
-  wrapperButtons: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-
-  button: {
-    marginLeft: 10,
-  },
-
 })
 
 export default CardsScreen
