318c8341508a357c0ab6dc26c7f552aaf45793e3
Gabriel <gabrielsarubo@hotmail.com>
2022-05-01 14:01:09 -0300

    refact: new model for the dataset MyCollections

--- src/pages/MyCollectionsScreen.js
+++ src/pages/MyCollectionsScreen.js
@@ -3,11 +3,11 @@ import { View, StyleSheet } from 'react-native'
 import CollectionsList from '../components/CollectionsList'
 
 const data_myCollections = [
-  { key: 0, title: 'Objetos', thumbnail: 'ball' },
-  { key: 1, title: 'Cores', thumbnail: 'colors' },
-  { key: 2, title: 'Animais', thumbnail: 'animals' },
-  { key: 3, title: 'Adjetivos', thumbnail: 'adjectives' },
-  { key: 4, title: 'Pronomes', thumbnail: 'pronouns' },
+  { key: 0, title: 'Objetos', thumbnail: 'ball', cardsList: [{ key: 0, front: 'Arvore', back: 'Tree', }, { key: 1, front: 'Janela', back: 'Window', },] },
+  { key: 1, title: 'Cores', thumbnail: 'colors', cardsList: [{ key: 0, front: 'Vermelho', back: 'Red', }, { key: 1, front: 'Laranja', back: 'Orange', }, { key: 2, front: 'Amarelo', back: 'Yellow', }, { key: 3, front: 'Verde', back: 'Green', }, { key: 4, front: 'Azul', back: 'Blue', }, { key: 5, front: 'Indigo', back: 'Indigo', },] },
+  { key: 2, title: 'Animais', thumbnail: 'animals', cardsList: [{ key: 0, front: 'Cavalo', back: 'Horse', }, { key: 1, front: 'Rato', back: 'Mouse', },] },
+  { key: 3, title: 'Adjetivos', thumbnail: 'adjectives', cardsList: [{ key: 0, front: 'Inefável', back: 'Inefable', }, { key: 1, front: 'Bonito', back: 'Pretty', },] },
+  { key: 4, title: 'Pronomes', thumbnail: 'pronouns', cardsList: [{ key: 0, front: 'Eles/elas', back: 'They', }, { key: 1, front: 'Nós', back: 'We', },] },
 ]
 
 const MyCollectionsScreen = ({ navigation }) => {
