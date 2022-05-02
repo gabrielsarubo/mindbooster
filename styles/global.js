665b862fdb977e84807c4856096161c2d4d3a0f7
Gabriel <gabrielsarubo@hotmail.com>
2022-05-02 17:49:46 -0300

    remove global styles

--- styles/global.js
+++ styles/global.js
@@ -1,18 +1,3 @@
 import { StyleSheet } from 'react-native'
 
-import colors from './colors'
-
-export const globalStyles = StyleSheet.create({
-  border: {
-    borderWidth: 1,
-    borderColor: 'red',
-  },
-  
-  bodyContainer: {
-    flex: 1,
-    justifyContent: 'space-between',
-    paddingHorizontal: 32,
-    backgroundColor: colors.darkPurple,
-    color: '#fdfdfd',
-  },  
-})
+export const globalStyles = StyleSheet.create({})
