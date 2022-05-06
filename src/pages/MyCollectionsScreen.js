bf0637b4f3b6ec6262c7175814d2f43a1c2c17e9
Gabriel Sousa <gabriel.lemes@mirumagency.com>
2022-05-05 21:20:53 -0300

    refact: set up CollectionContext

--- src/pages/MyCollectionsScreen.js
+++ src/pages/MyCollectionsScreen.js
@@ -1,3 +1,4 @@
+import { useContext } from 'react'
 import { View, StyleSheet } from 'react-native'
 
 import { globalStyles } from '../../styles/global'
@@ -5,19 +6,16 @@ import { globalStyles } from '../../styles/global'
 import CollectionsList from '../components/CollectionsList'
 import CustomFloatingButton from '../components/CustomFloatingButton'
 
-const data_myCollections = [
-  { key: 0, title: 'Objetos', thumbnail: 'ball', cardsList: [{ key: 0, front: 'Arvore', back: 'Tree', }, { key: 1, front: 'Janela', back: 'Window', },] },
-  { key: 1, title: 'Cores', thumbnail: 'colors', cardsList: [{ key: 0, front: 'Vermelho', back: 'Red', }, { key: 1, front: 'Laranja', back: 'Orange', }, { key: 2, front: 'Amarelo', back: 'Yellow', }, { key: 3, front: 'Verde', back: 'Green', }, { key: 4, front: 'Azul', back: 'Blue', }, { key: 5, front: 'Indigo', back: 'Indigo', },] },
-  { key: 2, title: 'Animais', thumbnail: 'animals', cardsList: [{ key: 0, front: 'Cavalo', back: 'Horse', }, { key: 1, front: 'Rato', back: 'Mouse', },] },
-  { key: 3, title: 'Adjetivos', thumbnail: 'adjectives', cardsList: [{ key: 0, front: 'Inefável', back: 'Inefable', }, { key: 1, front: 'Bonito', back: 'Pretty', },] },
-  { key: 4, title: 'Pronomes', thumbnail: 'pronouns', cardsList: [{ key: 0, front: 'Eles/elas', back: 'They', }, { key: 1, front: 'Nós', back: 'We', },] },
-]
+import { CollectionContext } from '../contexts/CollectionContext'
 
 const MyCollectionsScreen = ({ navigation }) => {
+  // TODO should this be inside the useEffect hook?
+  const { collections } = useContext(CollectionContext)
+
   return (
     <View style={styles.container}>
       <CollectionsList
-        data={data_myCollections}
+        data={collections}
         onPressHandler={(collection) => {
           navigation.navigate('Collection', {
             collection: collection
