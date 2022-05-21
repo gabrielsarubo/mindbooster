5a62d59a914c4763fc2246e1cbc6e2c5b567e608
Gabriel Sousa <gabriel.lemes@mirumagency.com>
2022-05-21 12:10:06 -0300

    set up redux devtools

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import 'react-native-gesture-handler'
 
 import { useState } from 'react'
+import { Provider } from 'react-redux'
+import { legacy_createStore as createStore } from 'redux'
+
+import { devToolsEnhancer } from 'redux-devtools-extension'
 
 import { StatusBar } from 'expo-status-bar'
 import { NavigationContainer } from '@react-navigation/native'
@@ -11,23 +15,29 @@ import AuthNavigator from './src/routes/AuthStack'
 
 import CollectionContextProvider from './src/contexts/CollectionContext'
 
+import rootReducer from './src/reducers'
+
+const store = createStore(rootReducer, devToolsEnhancer())
+
 const App = () => {
   const [userToken, setUserToken] = useState(false)
-  
+
   return (
-    <CollectionContextProvider>
-      <SafeAreaProvider>
-        <StatusBar style='light' />
-        <NavigationContainer>
-          {
-            userToken
-              ? <MainNavigator />
-              : <AuthNavigator />
-          }
-        </NavigationContainer>
-      </SafeAreaProvider>
-    </CollectionContextProvider>
+    <Provider store={store}>
+      <CollectionContextProvider>
+        <SafeAreaProvider>
+          <StatusBar style='light' />
+          <NavigationContainer>
+            {
+              userToken
+                ? <MainNavigator />
+                : <AuthNavigator />
+            }
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </CollectionContextProvider>
+    </Provider>
   )
 }
- 
+
 export default App
