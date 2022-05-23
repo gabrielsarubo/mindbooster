0bc396bb55fdc4066b79d31fb4969126628c4f36
Gabriel <gabrielsarubo@hotmail.com>
2022-05-22 22:12:14 -0300

    connect Redux to Firebase

--- src/store/store.js
+++ src/store/store.js
@@ -8,9 +8,18 @@ import thunk from 'redux-thunk'
 import { devToolsEnhancer } from 'redux-devtools-extension'
 // Reducers
 import rootReducer from './reducers'
+// Firebase Libs to Connect to Redux
+import { reduxFirestore, getFirestore } from 'redux-firestore'
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
+import fbConfig from '../config/firebase'
 
 export const store = createStore(
   rootReducer,
   {},
-  compose(applyMiddleware(thunk), devToolsEnhancer())
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reduxFirestore(fbConfig),
+    reactReduxFirebase(fbConfig),
+    devToolsEnhancer()
+  )
 )
