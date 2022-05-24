5c8fa068067cd978beacbbcc1383e39dededa989
Gabriel Sousa <gabriel.lemes@mirumagency.com>
2022-05-23 22:35:13 -0300

    update data model for a new Collection

--- src/store/actions/collectionActions.js
+++ src/store/actions/collectionActions.js
@@ -1,20 +1,22 @@
 import firebase from "../../config/firebase"
 
 export const createCollection = (collection, {uri, filename}) => {
-  return (dispatch) => {
+  return (dispatch, getState) => {
+    const { user } = getState()
+    
     // Reference the Firestore database and Storage
     const firestore = firebase.firestore()
     const storage = firebase.storage()
 
     // Make async call to Firebase Storage
     storage.ref(`images/${filename}`).putString(uri, 'data_url', { contentType: 'image/png'})
-      .then(snapshot => {
-        console.log(snapshot)
+      .then(() => {
         // Make async call to Firebase Firestore
         firestore.collection('collections').add({
           ...collection,
-          image: filename,
-          userId: 'dummy-user-id'
+          userId: user.uid,
+          createAt: new Date(),
+          thumbnail: filename,
         })
           .then(() => {
             dispatch({
